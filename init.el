33527e184a98a0344ad75161b3383b9fa7326e61
david.marques <david.marques@closer.pt>
2020-04-26 14:52:57 +0100

    Added font and theme

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(hydra dashboard company company-mode telephone-line evil-org evil-magit beacon which-key use-package)))
+   '(zenburn-theme zenburn hydra dashboard company company-mode telephone-line evil-org evil-magit beacon which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
